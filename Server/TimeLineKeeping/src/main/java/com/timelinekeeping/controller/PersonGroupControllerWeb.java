a8e5886f546e1a1f475a6710d0bbd9cf707c00a9
quanghien.working <quanghien.working@gmail.com>
2016-09-09 04:22:50 +0700

    comit API

--- Server/TimeLineKeeping/src/main/java/com/timelinekeeping/controller/PersonGroupControllerWeb.java
+++ Server/TimeLineKeeping/src/main/java/com/timelinekeeping/controller/PersonGroupControllerWeb.java
@@ -1,11 +1,11 @@
 package com.timelinekeeping.controller;
 
-import com.timelinekeeping.entity.PersonGroup;
-import com.timelinekeeping.service.PersonGroupService;
+import com.timelinekeeping.model.BaseResponse;
+import com.timelinekeeping.service.serviceImplement.PersonGroupServiceImpl;
+import com.timelinekeeping.util.JsonUtil;
+import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.List;
-import java.util.logging.Logger;
 
 /**
  * Created by lethanhtan on 9/7/16.
@@ -24,47 +22,62 @@ import java.util.logging.Logger;
 @Controller
 public class PersonGroupControllerWeb {
 
+    private Logger logger = Logger.getLogger(PersonGroupControllerWeb.class);
 
-    @RequestMapping(value = {"/persongroups"}, method = RequestMethod.POST)
+    @Autowired
+    private PersonGroupServiceImpl groupService;
+
+    @RequestMapping(value = {"/persongroups/create"}, method = RequestMethod.POST)
     @ResponseBody
-    public JSONObject create(@RequestParam("groupName") String groupName,
-                         @RequestParam("descriptions") String descriptions) {
-        PersonGroupService groupService = new PersonGroupService();
+    public BaseResponse create(@RequestParam("groupName") String groupName,
+                               @RequestParam("descriptions") String descriptions) {
         try {
-            JSONObject response = groupService.create(groupName, descriptions);
-            System.out.println("RESPONSE: " + response);
-
+            BaseResponse response = groupService.create(groupName, descriptions);
+            logger.info("RESPONSE: " + JsonUtil.toJson(response));
             return response;
-        } catch (URISyntaxException e) {
-            System.out.println(e.getMessage());
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("error", e.getMessage());
-            return jsonObject;
 
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("error", e.getMessage());
-            return jsonObject;
+        } catch (Exception e) {
+            logger.error(e);
+            return new BaseResponse(e);
         }
     }
 
     @RequestMapping(value = {"/persongroups/listAll"}, method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity<List<PersonGroup>> listAll(@RequestParam("start") int start,
-                                                     @RequestParam("top") int top) {
-        PersonGroupService groupService = new PersonGroupService();
+    public BaseResponse listAll(@RequestParam("start") int start,
+                                @RequestParam("top") int top) {
         try {
-            List<PersonGroup> personGroupList = groupService.listAll(start, top);
-            return new ResponseEntity<>(personGroupList, HttpStatus.OK);
+//            List<PersonGroup> personGroupList = groupService.listAll(start, top);
+//            return new ResponseEntity<>(personGroupList, HttpStatus.OK);
+            return groupService.listAll(start, top);
 
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            logger.error(e);
+            return new BaseResponse(e);
         }
-
-        return null;
     }
+
+    @RequestMapping(value = {"/persongroups/train"}, method = RequestMethod.GET)
+    @ResponseBody
+    public BaseResponse trainPersonGroup(@RequestParam("person_groups_id") String persongroupid) {
+        try {
+            return groupService.trainGroup(persongroupid);
+        } catch (Exception e) {
+            logger.error(e);
+            return new BaseResponse(e);
+        }
+    }
+
+    @RequestMapping(value = {"/persongroups/training_status"}, method = RequestMethod.GET)
+    @ResponseBody
+    public BaseResponse trainPersonGroupStatus(@RequestParam("person_groups_id") String persongroupid) {
+        try {
+            return groupService.trainPersonGroupStatus(persongroupid);
+        } catch (Exception e) {
+            logger.error(e);
+            return new BaseResponse(e);
+        }
+    }
+
+
 }
