5629f70f81915e39421b5ada8e245c1079eb744d
trungnn1728 <trungnn1728@gmail.com>
2016-09-18 01:51:24 +0700

    [Trung] - Add department

--- Server/TimeLineKeeping/src/main/java/com/timelinekeeping/controller/DepartmentControllerWeb.java
+++ Server/TimeLineKeeping/src/main/java/com/timelinekeeping/controller/DepartmentControllerWeb.java
@@ -10,8 +10,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 /**
  * Created by TrungNN on 9/17/2016.
@@ -29,7 +32,7 @@ public class DepartmentControllerWeb {
     public String loadManagementDepartmentView(Model model) {
         int page = 0;
         int size = 1000;
-        
+
         BaseResponse response = departmentService.findAll(page, size);
         Page<DepartmentEntity> pageDepartment = (Page<DepartmentEntity>) response.getData();
 
@@ -37,5 +40,46 @@ public class DepartmentControllerWeb {
         model.addAttribute("ListDepartments", pageDepartment.getContent());
 
         return "/views/admin/management_depart/management_depart";
+    }
+
+    @RequestMapping(value = "/addDepartment", method = RequestMethod.GET)
+    public String loadAddDepartmentView() {
+        return "/views/admin/management_depart/add_depart";
+    }
+
+    @RequestMapping(value = "/addDepartmentProcessing", method = RequestMethod.POST)
+    public String addDepartment(@RequestParam("departmentCode") String departmentCode,
+                                @RequestParam("departmentName") String departmentName,
+                                @RequestParam("description") String description,
+                                Model model) {
+        logger.info("[Add Department] BEGIN");
+        logger.info("[Add Department] department code: " + departmentCode);
+        logger.info("[Add Department] department name: " + departmentName);
+        logger.info("[Add Department] description: " + description);
+        boolean active = true;
+        DepartmentEntity departmentEntity
+                = new DepartmentEntity(departmentCode, departmentName, description, active);
+
+        boolean success = false;
+        String url = "/views/admin/management_depart/add_depart";
+        try {
+            BaseResponse response = departmentService.create(departmentEntity);
+            success = response.isSuccess();
+        } catch (IOException e) {
+            logger.info("[Add Department] IOException: " + e.getMessage());
+        } catch (URISyntaxException e) {
+            logger.info("[Add Department] URISyntaxException: " + e.getMessage());
+        }
+
+        logger.info("[Add Department] success: " + success);
+        if (success) {
+            url = "redirect:/admin/departments/";
+        } else {
+            model.addAttribute("departmentCode", departmentCode);
+            model.addAttribute("departmentName", departmentName);
+            model.addAttribute("description", description);
+        }
+        logger.info("[Add Department] END");
+        return url;
     }
 }
