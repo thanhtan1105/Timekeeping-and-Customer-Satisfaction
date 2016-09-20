9ae34c8d749fcd22bfb1ad2fdf3c2c7361fbcb7c
quanghien.working <quanghien.working@gmail.com>
2016-09-20 15:49:43 +0700

    commit edit enity

--- Server/TimeLineKeeping/src/main/java/com/timelinekeeping/repository/AccountRepo.java
+++ Server/TimeLineKeeping/src/main/java/com/timelinekeeping/repository/AccountRepo.java
@@ -22,9 +22,9 @@ public interface AccountRepo extends JpaRepository<AccountEntity, Long> {
     @Query(value = "SELECT * FROM account a WHERE a.department_id = ?1 LIMIT ?2, ?3", nativeQuery = true)
     List<AccountEntity> findByDepartment(int departmentId, int start, int top);
 
-    @Query("SELECT a FROM account a WHERE a.username = ?1 and a.active <>0")
+    @Query("SELECT a FROM AccountEntity a WHERE a.username = ?1 and a.active <>0")
     AccountEntity findByUsername(String username);
 
-    @Query("SELECT a FROM account a WHERE a.userCode = ?1 and a.active <>0")
+    @Query("SELECT a FROM AccountEntity a WHERE a.userCode = ?1 and a.active <>0")
     AccountEntity findByUsercode(String code);
 }
