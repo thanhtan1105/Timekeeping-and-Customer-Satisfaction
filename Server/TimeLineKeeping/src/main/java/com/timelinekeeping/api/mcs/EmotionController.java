2ffe37884c9388862abc39ec58c48ae355bc805c
quanghien.working <quanghien.working@gmail.com>
2016-09-21 13:34:56 +0700

    commit refactor

--- Server/TimeLineKeeping/src/main/java/com/timelinekeeping/api/mcs/EmotionController.java
+++ Server/TimeLineKeeping/src/main/java/com/timelinekeeping/api/mcs/EmotionController.java
@@ -51,8 +51,8 @@ public class EmotionController {
             logger.info(IContanst.BEGIN_METHOD_CONTROLLER + Thread.currentThread().getStackTrace()[1].getMethodName());
             BaseResponse response;
             if (ValidateUtil.isImageFile(imgFile.getInputStream())) {
-                response = emotionService.save(imgFile.getInputStream(), employeeId, isFirstTime);
-//                response = emotionServiceMCS.recognize(imgFile.getInputStream());
+//                response = emotionService.save(imgFile.getInputStream(), employeeId, isFirstTime);
+                response = emotionServiceMCS.recognize(imgFile.getInputStream());
             } else {
                 response = new BaseResponse();
                 response.setSuccess(false);
@@ -67,39 +67,4 @@ public class EmotionController {
         }
     }
 
-    @RequestMapping(value = {"/analyse_emotion"}, method = RequestMethod.POST)
-    @ResponseBody
-    public BaseResponse analyseEmotion(@RequestParam("id") Long id) {
-        logger.info(IContanst.BEGIN_METHOD_CONTROLLER + Thread.currentThread().getStackTrace()[1].getMethodName());
-        BaseResponse baseResponse = emotionService.analyseEmotion(id);
-        logger.info(IContanst.END_METHOD_CONTROLLER);
-        return baseResponse;
-    }
-
-    @RequestMapping(value = {"/get_customer_emotion"}, method = RequestMethod.POST)
-    @ResponseBody
-    public BaseResponse getCustomerEmotion(@RequestParam("img") MultipartFile imgFile,
-                                           @RequestParam("employeeId") long employeeId,
-                                           @RequestParam("isFirstTime") boolean isFirstTime) {
-        logger.info(IContanst.BEGIN_METHOD_CONTROLLER + Thread.currentThread().getStackTrace()[1].getMethodName());
-        BaseResponse response;
-        try {
-            if (ValidateUtil.isImageFile(imgFile.getInputStream())) {
-                response = emotionService.getCustomerEmotion(imgFile.getInputStream(), employeeId, isFirstTime);
-            } else {
-                response = new BaseResponse();
-                response.setSuccess(false);
-                response.setMessage("File not image format.");
-            }
-            return response;
-        } catch (IOException e) {
-            logger.error(e);
-            return new BaseResponse(e);
-        } catch (URISyntaxException e) {
-            logger.error(e);
-            return new BaseResponse(e);
-        } finally {
-            logger.info(IContanst.END_METHOD_CONTROLLER);
-        }
-    }
 }
