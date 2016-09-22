c18c0edf1455c9f5f78b06687a56ad7f708ef045
trungnn1728 <trungnn1728@gmail.com>
2016-09-22 23:48:34 +0700

    [Trung] - layouts web: management reminder + add reminder

---
+++ Server/TimeLineKeeping/src/main/java/com/timelinekeeping/constant/ViewConst.java
@@ -0,0 +1,14 @@
+package com.timelinekeeping.constant;
+
+/**
+ * Created by TrungNN on 9/22/2016.
+ */
+public interface ViewConst {
+
+    /**
+     * Views for manager
+     */
+    String MANAGEMENT_REMINDER_VIEW = "/views/manager/management_reminder/management_reminder";
+    String ADD_REMINDER_VIEW = "/views/manager/management_reminder/add_reminder";
+}
+
