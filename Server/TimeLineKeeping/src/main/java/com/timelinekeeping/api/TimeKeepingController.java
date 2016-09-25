6b94d0b2e419704e72cb7a2f4e707056071883e8
quanghien.working <quanghien.working@gmail.com>
2016-09-26 01:22:52 +0700

    commit edit code

--- Server/TimeLineKeeping/src/main/java/com/timelinekeeping/api/TimeKeepingController.java
+++ Server/TimeLineKeeping/src/main/java/com/timelinekeeping/api/TimeKeepingController.java
@@ -26,7 +26,8 @@ public class TimeKeepingController {
     private Logger logger = LogManager.getLogger(TimeKeepingController.class);
 
     @RequestMapping(I_URI.API_TIMEKEEPING_LIST_EMPLOYEE)
-    public List<AccountCheckInModel> getEmployeeDepartment(@RequestParam("departmentId") Long departmentId){
+    public List<AccountCheckInModel> getEmployeeDepartment(@RequestParam("departmentId") Long departmentId,
+                                                           @RequestParam("accountId") Long accountId){
         try {
             logger.info(IContanst.BEGIN_METHOD_CONTROLLER + Thread.currentThread().getStackTrace()[1].getMethodName());
             return timekeepingService.getEmployeeDepartment(departmentId, accountId);
@@ -62,7 +63,7 @@ public class TimeKeepingController {
 
 
 
-    @RequestMapping(value = I_URI.API_TIMEKEEPING_CHECK_IN_MANUAL, method = RequestMethod.POST)
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
     public List<CheckinManualModel> getTimeKeeping(@RequestParam(value = "accountId", required = false) List<CheckinManualModel> listCheckin){
         try {
             logger.info(IContanst.BEGIN_METHOD_CONTROLLER + Thread.currentThread().getStackTrace()[1].getMethodName());
