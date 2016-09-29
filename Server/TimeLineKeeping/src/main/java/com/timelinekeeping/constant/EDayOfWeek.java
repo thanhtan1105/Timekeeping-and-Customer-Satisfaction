0a0923e988ab0df63b55705ed8aeaab1131b1d41
quanghien.working <quanghien.working@gmail.com>
2016-09-29 23:22:12 +0700

    [HienTQ] commit apiTimekeeping

---
+++ Server/TimeLineKeeping/src/main/java/com/timelinekeeping/constant/EDayOfWeek.java
@@ -0,0 +1,57 @@
+package com.timelinekeeping.constant;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.time.DayOfWeek;
+
+/**
+ * Created by lethanhtan on 9/15/16.
+ */
+public enum EDayOfWeek {
+
+    SUNDAY(1, "SUNDAY"),
+    MONDAY(2, "MONDAY"),
+    TUESDAY(3, "TUESDAY"),
+    WEDNESDAY(4, "WEDNESDAY"),
+    THURSDAY(5, "THURSDAY"),
+    FRIDAY(6, "FRIDAY"),
+    SATURDAY(7, "SATURDAY");
+
+    private int index;
+    private String name;
+
+    EDayOfWeek(int index, String name) {
+        this.index = index;
+        this.name = name;
+    }
+
+    public static EDayOfWeek fromIndex(int index){
+        for (EDayOfWeek ex : values()){
+            if (ex.getIndex() == index){
+                return ex;
+            }
+        }
+        return null;
+    }
+
+    public static EDayOfWeek fromIndex(DayOfWeek day){
+
+    }
+
+    @JsonValue
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
