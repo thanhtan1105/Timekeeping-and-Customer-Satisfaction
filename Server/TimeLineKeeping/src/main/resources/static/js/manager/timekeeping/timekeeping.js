0920afaadf56b6e02289246cc1647a2fafecc545
trungnn1728 <trungnn1728@gmail.com>
2016-09-30 17:24:16 +0700

    [Trung] - Code web: view details timekeeping

---
+++ Server/TimeLineKeeping/src/main/resources/static/js/manager/timekeeping/timekeeping.js
@@ -0,0 +1,23 @@
+/**
+ * Created by TrungNN on 9/30/2016.
+ */
+
+/**
+ * Submit form view details
+ */
+$('.view-details-tk').on('click', function () {
+    var id = $(this).attr('data-id'),
+        $form_submit_details = $('#form-submit-view-details-' + id),
+        accountTKDetailsModel = $form_submit_details.find('[name="accountTKDetailsModel"]'),
+        accountId = id,
+        selectedDate = $('.selected-date').val(),
+        accountTKDetailsModelJson = '{';
+    accountTKDetailsModelJson += '"accountId":' + accountId;
+    accountTKDetailsModelJson += ', "selectedDate":"' + selectedDate + '"';
+    accountTKDetailsModelJson += '}';
+
+    accountTKDetailsModel.val(accountTKDetailsModelJson);
+
+    // submit form
+    $form_submit_details.submit();
+});
