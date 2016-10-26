4edd3d65b39a64109194d708d9307fbe4f44d0cf
TrungNN <trungnn1728@gmail.com>
2016-10-26 16:35:07 +0700

    update time reminder

--- Server/TimeLineKeeping/src/main/resources/static/js/manager/reminder/management_reminder.js
+++ Server/TimeLineKeeping/src/main/resources/static/js/manager/reminder/management_reminder.js
@@ -9,10 +9,22 @@
 $('.btn-edit-reminder').on('click', function () {
     var id = $(this).attr('data-id'),
         $form_submit_view_reminder = $('#form-submit-view-reminder'),
-        reminderId = $form_submit_view_reminder.find('[name="reminderId"]');
-    console.info('id: ' + id);
-    reminderId.val(id);
+        reminderId = $form_submit_view_reminder.find('[name="reminderId"]'),
+        $text_time_reminder = $('#text-time-reminder-' + id).val();
+    console.info('[id] ' + id);
+    console.info('[text time reminder] ' + $text_time_reminder);
 
-    //submit form
-    $form_submit_view_reminder.submit();
+    //check (time_reminder - current_time) >= 15 minutes
+    var time_reminder = new Date($text_time_reminder);
+    var current_time = new Date();
+    console.info('[time reminder] ' + time_reminder);
+    console.info('[current time] ' + current_time);
+    var difference_time = time_reminder - current_time;
+    console.info('[difference time] ' + difference_time);
+    if (difference_time >= time_out_reminder) {
+        reminderId.val(id);
+
+        //submit form
+        $form_submit_view_reminder.submit();
+    }
 })
