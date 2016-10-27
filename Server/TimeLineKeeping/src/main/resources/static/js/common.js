a74163725f1807f766eaa666223036eb3cc95bf1
TrungNN <trungnn1728@gmail.com>
2016-10-27 19:54:14 +0700

    commit

--- Server/TimeLineKeeping/src/main/resources/static/js/common.js
+++ Server/TimeLineKeeping/src/main/resources/static/js/common.js
@@ -8,3 +8,22 @@ var time_out = 2500;
  * @type {number}
  */
 var time_out_reminder = 1000 * 60 * 15;
+
+/**
+ * Fc: format date
+ * @param date
+ * @returns {string}
+ */
+function formatDate(date) {
+    var year = date.getFullYear(),
+        month = date.getMonth() + 1, // months are zero indexed
+        day = date.getDate(),
+        hour = date.getHours(),
+        minute = date.getMinutes(),
+        second = date.getSeconds(),
+        hourFormatted = hour % 24 || 24, // hour returned in 24 hour format
+        minuteFormatted = minute < 10 ? "0" + minute : minute;
+
+
+    return day + "/" + month + "/" + year + " (" + hourFormatted + ":" + minuteFormatted + ")";
+}
