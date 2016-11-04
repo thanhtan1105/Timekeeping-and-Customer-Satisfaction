40b46a6e9597ded269f6e3f565bf9a786c8c7e95
quanghien.working <quanghien.working@gmail.com>
2016-11-04 16:41:15 +0700

    commit api model

--- Server/TimeLineKeeping/src/main/java/com/timelinekeeping/model/EmotionCompare.java
+++ Server/TimeLineKeeping/src/main/java/com/timelinekeeping/model/EmotionCompare.java
@@ -7,11 +7,21 @@ import com.timelinekeeping.constant.EEmotion;
  */
 public class EmotionCompare {
     private EEmotion emotion;
+    private String emotionName;
     private Double value;
+    private Double percent;
 
     public EmotionCompare(EEmotion emotion, Double value) {
         this.emotion = emotion;
         this.value = value;
+        this.emotionName = emotion.getName();
+    }
+
+    public EmotionCompare(EEmotion emotion, Double value, Double percent) {
+        this.emotion = emotion;
+        this.value = value;
+        this.percent = percent;
+        this.emotionName = emotion.getName();
     }
 
     public EEmotion getEmotion() {
@@ -20,6 +30,7 @@ public class EmotionCompare {
 
     public void setEmotion(EEmotion emotion) {
         this.emotion = emotion;
+        this.emotionName = emotion.getName();
     }
 
     public Double getValue() {
@@ -28,6 +39,22 @@ public class EmotionCompare {
 
     public void setValue(Double value) {
         this.value = value;
+    }
+
+    public Double getPercent() {
+        return percent * 100;
+    }
+
+    public void setPercent(Double percent) {
+        this.percent = percent;
+    }
+
+    public String getEmotionName() {
+        return emotionName;
+    }
+
+    public void setEmotionName(String emotionName) {
+        this.emotionName = emotionName;
     }
 
     @Override
