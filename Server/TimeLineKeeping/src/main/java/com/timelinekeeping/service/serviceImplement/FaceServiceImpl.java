9340cc923eb036794ce01f12b566ebd4728e1805
Le Thanh Tan <thanhtan1105@gmail.com>
2016-11-08 00:42:53 +0700

    remove face function

--- Server/TimeLineKeeping/src/main/java/com/timelinekeeping/service/serviceImplement/FaceServiceImpl.java
+++ Server/TimeLineKeeping/src/main/java/com/timelinekeeping/service/serviceImplement/FaceServiceImpl.java
@@ -98,6 +98,7 @@ public class FaceServiceImpl {
 
         BaseResponse faceResponse = HTTPClientUtil.getInstanceFace().toGet(new URI(url), String.class);
         return faceResponse;
+
     }
 }
 
