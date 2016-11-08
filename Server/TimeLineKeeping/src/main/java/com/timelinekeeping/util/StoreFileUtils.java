76b3843b4bfd2691a7421648460721b32ee38985
quanghien.working <quanghien.working@gmail.com>
2016-11-08 17:38:54 +0700

    commit refactor

--- Server/TimeLineKeeping/src/main/java/com/timelinekeeping/util/StoreFileUtils.java
+++ Server/TimeLineKeeping/src/main/java/com/timelinekeeping/util/StoreFileUtils.java
@@ -6,6 +6,8 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.imageio.ImageIO;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.*;
 
@@ -32,6 +34,7 @@ public class StoreFileUtils {
             return fileName;
         } catch (IOException e) {
             e.printStackTrace();
+            System.out.println(e.getStackTrace());
             return null;
         }
     }
@@ -42,6 +45,31 @@ public class StoreFileUtils {
             if (file.exists()){
                 file.delete();
             }
+        }
+    }
+
+    public static byte[] rotateImage(InputStream stream) {
+        try {
+            BufferedImage bufferedImage = ImageIO.read(stream);
+            double rotationRequired = Math.toRadians(90);
+            double locationX = bufferedImage.getHeight() / 2;
+            double locationY = bufferedImage.getWidth() / 2;
+            AffineTransform transform = AffineTransform.getRotateInstance(rotationRequired, locationX, locationY);
+
+            AffineTransformOp affineTransformOp = new AffineTransformOp(transform, AffineTransformOp.TYPE_BILINEAR);
+            int maximun = (int) (Math.max(bufferedImage.getHeight(), bufferedImage.getWidth()) * 1.20);
+            BufferedImage newImage = new BufferedImage(maximun, maximun, bufferedImage.getType());
+            affineTransformOp.filter(bufferedImage, newImage);
+//            g2d.drawImage(op.filter(image, null), drawLocationX, drawLocationY, null);
+
+
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            ImageIO.write(newImage, IContanst.EXTENSION_FILE_IMAGE, outputStream);
+            byte[] byteImage = outputStream.toByteArray();
+            return byteImage;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 
