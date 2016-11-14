41d162edad163c26fa74dba41f156bbc3ac026cc
quanghien.working <quanghien.working@gmail.com>
2016-11-14 16:55:40 +0700

    commit update

--- Server/TimeLineKeeping/src/main/java/com/timelinekeeping/api/HandlerController.java
+++ Server/TimeLineKeeping/src/main/java/com/timelinekeeping/api/HandlerController.java
@@ -1,6 +1,7 @@
 package com.timelinekeeping.api;
 
 import com.timelinekeeping.common.BaseResponse;
+import com.timelinekeeping.common.Pair;
 import com.timelinekeeping.constant.IContanst;
 import com.timelinekeeping.constant.I_URI;
 import com.timelinekeeping.model.ConfigurationModel;
@@ -8,11 +9,10 @@ import com.timelinekeeping.model.HistoryModel;
 import com.timelinekeeping.service.serviceImplement.HandlerServiceImpl;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import com.timelinekeeping.constant.I_URI;
-import com.timelinekeeping.service.serviceImplement.HandlerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -78,6 +78,20 @@ public class HandlerController {
             } else {
                 return new BaseResponse(true, result);
             }
+        } catch (Exception e) {
+            logger.error(IContanst.LOGGER_ERROR, e);
+            return new BaseResponse(e);
+        } finally {
+            logger.info(IContanst.END_METHOD_CONTROLLER + Thread.currentThread().getStackTrace()[1].getMethodName());
+        }
+    }
+
+    @RequestMapping(value = I_URI.API_HANDLER_UPDATE_CONFIGURATION, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public BaseResponse updateConfiguration(@RequestParam("key") String key,
+                                            @RequestParam("value") String value) {
+        try {
+            logger.info(IContanst.BEGIN_METHOD_CONTROLLER + Thread.currentThread().getStackTrace()[1].getMethodName());
+            return new BaseResponse(handlerService.updateConfiguration(new Pair<String, String>(key, value)));
         } catch (Exception e) {
             logger.error(IContanst.LOGGER_ERROR, e);
             return new BaseResponse(e);
