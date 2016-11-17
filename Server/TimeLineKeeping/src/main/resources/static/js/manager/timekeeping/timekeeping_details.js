96d72d78ec75b1828373aec0de5191c2a0440ea7
trungnn1728 <trungnn1728@gmail.com>
2016-11-17 16:54:41 +0700

    show image employee checkin by camera in timekeeping report

--- Server/TimeLineKeeping/src/main/resources/static/js/manager/timekeeping/timekeeping_details.js
+++ Server/TimeLineKeeping/src/main/resources/static/js/manager/timekeeping/timekeeping_details.js
@@ -14,7 +14,7 @@ $('#date-picker-timekeeping-detail').datepicker({
 /**
  * Event: select another month
  */
-$('.selected-date').on('change', function() {
+$('.selected-date').on('change', function () {
     var selectedDate = $('.selected-date').val(),
         $form_submit_change_month = $('#form-submit-change-month'),
         selectedMonth = $form_submit_change_month.find('[name="selectedMonth"]');
@@ -23,3 +23,47 @@ $('.selected-date').on('change', function() {
     // submit form
     $form_submit_change_month.submit();
 });
+
+function show_image_employee(src) {
+    console.info('[show img emp][src] ' + src);
+    if (src != null) {
+        //set content image
+        set_src_img(src, '#img-employee');
+        //show modal
+        show_modal('#modal-img-employee', true);
+    } else {
+        //do nothing
+    }
+}
+
+/**
+ * Fc: set source image
+ * @param src
+ * @param id
+ */
+function set_src_img(src, id) {
+    $(id).attr('src', src);
+}
+
+/**
+ * For common fc
+ */
+/**
+ * Fc: show modal
+ * @param id
+ * @param enabled ('true', 'false')
+ */
+function show_modal(id, keyboard) {
+    if (!keyboard) {//prevent closing
+        $(id).modal({
+            backdrop: 'static',
+            keyboard: keyboard, //(false) prevent closing with Esc button
+            show: true
+        });
+    } else {//allow closing
+        $(id).modal({
+            show: true
+        });
+    }
+
+}
